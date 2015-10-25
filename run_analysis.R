48b8aec3bcea39e06c5989f209db2190e24d7f53
bohnen <bohnen.net@gmail.com>
2015-10-25 20:19:56 +0900

    using dplyr

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,17 @@
-## read constants
+library(dplyr)
+
+## constants
 data_dir  <- "UCI HAR Dataset"
+
+## if there isn't data file, download and unzip it.
+if(!file.exists(data_dir)){
+  download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+                "data.zip",
+                method="curl")
+  unzip("data.zip")
+}
+
+## read constants
 features  <- read.table(sprintf("%s/features.txt",data_dir))
 act_label <- read.table(sprintf("%s/activity_labels.txt",data_dir))
 
@@ -14,20 +26,27 @@ readData <- function(dir, spec="test"){
   activity <- merge(activity, act_label,sort=FALSE)$V2
   names(subject)  <- "subject"
   names(activity) <- "activity"
+  
   ## labels the data set with descriptive variable names (4.)
-  names(data)     <- features$V2
+  ## R's data name doesn't accept "-" and "()", so replace it.
+  names(data)     <- gsub("\\(\\)","",gsub("-","_",features$V2))
   
   ## extracts only mean and standard deviation (2.)
-  data     <- data[,grep("mean\\(\\)|std\\(\\)",features$V2,value=TRUE)]
+  ## grep patterns are carefully choosed for excluding "*meanFreq"  
+  data     <- data[,grep("_mean_|_std_|_mean$|_std$",names(data),value=TRUE)]
   
   data.frame(activity, subject, data)
 }
 
 ## merge train and test data (1.)
-all_data = rbind(readData(data_dir,"train"),
+all_data <- rbind(readData(data_dir,"train"),
                  readData(data_dir,"test"))
 
 ## creates tidy data set with the average of each variable
 ## for each activity and each subject
-tidy_data = aggregate(. ~ activity + subject, all_data, mean)
+tidy_data <- tbl_df(all_data) %>%
+  group_by(activity,subject) %>%
+  summarise_each(funs(mean)) %>%
+  arrange(activity,subject)
+
 write.table(tidy_data,"tidy_data.txt",row.name = FALSE)
